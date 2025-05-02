cf5144fa94c18ff0cd810e962d112cfdc1eb9475
albertirgi <irgialbert@gmail.com>
2025-05-02 10:29:15 +0700

    base: program

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main>
+        <div>
+            <h1 className="text-3xl underline">Welcome to My Portfolio</h1>
+        </div>
+    </main>
+  )
+}
+
+export default App
